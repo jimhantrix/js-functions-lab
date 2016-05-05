36b7c23c5669e5c934fdffac6473a66c0e9bf866
jimhantrix <jimtekanyo@gmail.com>
2016-05-04 23:04:10 -0700

    updating lab

--- starter-code/functions.js
+++ starter-code/functions.js
@@ -1,52 +1,109 @@
+
 // Question 1
-function maxOfTwoNumbers() {
+function maxOfTwoNumbers(a,b) {
+ if (a > b){
+
+    return a;
+
+ } else if (b > a){
+
+   return b;
+  }
 
 }
 
 // Question 2
-function maxOfThree() {
-
+function maxOfThree(x,y,z) {
+if (x > y && x > z){
+    return x;
 }
+if (y > x && y > z )
+  return y;
+}
+
+if (z > x && z > y){
+
+    return z;
+   }
+}
+
 
 // Question 3
-function isCharacterAVowel() {
+function isCharacterAVowel(x) {
+
+var vowels = ['j','i','m','T','k'];
+  if (vowels.indexOf(c) !== -1) {
+    return true;
+  }
+  return false;
+}
+
+
 
 }
 
 // Question 4
-function sumArray() {
- 
+function sumArray(arr) {
+  var sum = 0;
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+
 }
 
 
 // Question 4
-function multiplyArray() {
 
+function multiplyArray(arr) {
+  var product = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+    product *= arr[i];
+  }
+  return product;
 }
+
+
 
 
 // Question 5
+
 var numberOfArguments = function(){
-  
+  return args.length
 }
 
 
 
 // Question 6
-var reverseString = function (){
-  
+
+reverseString = function(){
+  return this.split('').reverse().join('');
 };
 
 
 // Question 7
-function findLongestWord () {
-  
+
+function findLongestWord (arr) {
+  longest = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i].length > longest) {
+      longest = arr[i].length;
+    }
+  }
+  return longest;
 }
 
-
 // Question 8
-function filterLongWords () {
-  
+function filterLongWords (arr,num) {
+  newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    if(arr[i].length > num){
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
 }
 
 
@@ -56,6 +113,5 @@ function filterLongWords () {
 
 // Bonus 2
 function charactersOccurencesCount() {
-  
-}
 
+}
